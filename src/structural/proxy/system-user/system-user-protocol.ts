a704569040be43d109437bbde63535440c9bfa57
Luk3rF7 <lkzeron@gmail.com>
2023-12-27 19:17:27 -0300

    delete

--- src/structural/proxy/system-user/system-user-protocol.ts
+++ src/structural/proxy/system-user/system-user-protocol.ts
@@ -1,4 +1,7 @@
-export type SystemUserAddressProtocol = { street: string; number: number };
+export type SystemUserAddressProtocol = {
+   street: string;
+   number: number
+  };
 
 export interface SystemUserProtocol {
   firstName: string;
